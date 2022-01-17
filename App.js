55f12cd70b4e8846e910ac38caab22d347ccb7a6
mattmoyka <mattmoyka@gmail.com>
2022-01-17 16:10:12 -0500

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+import { StatusBar, StyleSheet, Text, View } from 'react-native';
+import Amplify from 'aws-amplify'
+import awsconfig from './src/aws-exports'
+Amplify.configure(awsconfig)
+import Home from './src/Home';
+import { withAuthenticator } from 'aws-amplify-react-native'
+import { Auth } from 'aws-amplify'
+
+async function signOut() {
+  try {
+    await Auth.signOut({ global: true });
+  } catch (error) {
+    console.log('error signing out: ', error);
+  }
+}
+
+function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar />
+      <Home />
+      <button onClick={signOut}>sign out</button>
     </View>
   );
 }
+
+export default withAuthenticator(App)
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
